a2e4893088dc63990abe7c5e427f3cdc196df98d
steven <steven21470@gmail.com>
2022-06-27 14:34:14 +0800

    max heap and max heap sort

---
+++ heap.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define maxsize 20
+//maxheap
+int size=-1;
+
+void swap(int*x,int*y){
+    int z=*y;
+    *y=*x;
+    *x=z;
+}
+
+int parent(int x){
+    return (x-1)/2;
+}
+
+int leftchild(int x){
+    return 2*x+1;
+}
+
+int rightchild(int x){
+    return 2*x+2;
+}
+
+void max_heapify(int *array ,int position){
+    int left=leftchild(position);
+    int right=rightchild(position);
+    int large=position;
+    if(left <= size && array[large] < array[left]){
+        large=left;
+    }
+    if(right <=size && array[large] < array[right]){
+        large = right;
+    }
+    if(large!=position){
+        swap(&array[large],&array[position]);
+        max_heapify(array,large);
+    }
+}
+
+void insert(int *array ,int data){
+    if(size==-1){
+        size++;
+        array[size]=data;
+        return;
+    }else if(size == maxsize){
+        printf("The heap is full");
+        return;
+    }else{
+        size++;
+        array[size]=data;
+        int i=size;
+        while(array[parent(i)]<array[i]){
+            swap(&array[parent(i)],&array[i]);
+            i=parent(i);
+        }
+    }
+}
+
+void delete(int *array,int data){
+    int flag=0;
+    int i=0;
+    for(i=0;i<=size;i++){
+        if(array[i]==data){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        printf("no data in array\n");
+        return;
+    }
+    swap(&array[i],&array[size]);
+    size--;
+    max_heapify(array,i);
+
+}
+
+void max_heap_sort(int *array,int *sort){
+    int index=size;
+    for(int i=0;i<=index;i++){
+        sort[i]=array[0];
+        delete(array,array[0]);
+        printf(" %d ",sort[i]);
+    }
+    printf("\n");
+}
+
+void printarray(int *array){
+    for(int i=0;i<=size;i++){
+        printf(" %d ",array[i]);
+    }
+    printf("\n");
+}
+
+int get_max(int *array){
+    return array[0];
+}
+int main(){
+    int array[maxsize];
+    int sort[maxsize];
+    insert(array,5);
+    insert(array,8);
+    insert(array,3);
+    insert(array,9);
+    insert(array,1);
+    insert(array,10);
+    insert(array,11);
+    insert(array,12);
+    insert(array,13);
+    printarray(array);
+
+    delete(array,13);
+    printarray(array);
+
+    printf(" max : %d\n",get_max(array));
+    //final to get sorted
+    max_heap_sort(array,sort);
+
+    insert(array,1);
+    insert(array,2);
+    insert(array,3);
+    insert(array,4);
+    insert(array,5);
+    insert(array,6);
+    insert(array,7);
+    insert(array,8);
+    insert(array,9);
+    insert(array,10);
+    printarray(array);
+    max_heap_sort(array,sort);
+
+}
